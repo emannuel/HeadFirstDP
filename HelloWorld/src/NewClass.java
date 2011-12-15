08b7d1a96bce9cb68af1cfd699599f3f99a909e5
emannuel <emannuel_rev@hotmail.com>
2011-12-14 22:20:16 -0200

    asdas

--- HelloWorld/src/NewClass.java
+++ HelloWorld/src/NewClass.java
@@ -2,10 +2,6 @@
 public class NewClass {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hi! ");  
-=======
-		System.out.println("Hi !");  
->>>>>>> refs/heads/iss54
 	}
 }
